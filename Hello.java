85db421a08ce2f6a70da16fb527f6d366aa5c256
Boolean694 <jwong01@stuy.edu>
2018-09-14 10:52:43 -0400

    Add Hello class

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void NameConstructor(String x,String y) {
+        System.out.println("Hello! My name is " + x + y + "!");
+    }
+    public static void main(String args[]) {
+        String x = "Julian ";
+        String y = "Wong";
+        NameConstructor(x,y);
+    }
+}
